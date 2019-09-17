9398e726e49b8b5f7787fa51584fdaf5c2b26251
Jonathan Dang <jdang@wpi.edu>
2019-09-17 11:41:05 -0400

    accessed my homepage

--- analyzer.py
+++ analyzer.py
@@ -4,18 +4,17 @@ import tweepy
 
 from tweepy import OAuthHandler
 
-consumer_key = '8y3U5ASNoOdwxReKKx4wMRCd0'
-consumer_secret = 'JjZKkPGNjBNN8n2pNLBDiwg3DHNEjUFPefEvgMNywk6SD6peRh'
-#access_token = ''
-#access_secret = ''
+consumer_key = '<insert key>'
+consumer_secret = '<insert secret>'
+access_token = '<insert token>'
+access_secret = '<insert secret>'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 if __name__ == "__main__" :
 	print("Twitter Sentiment Analyzer\n")
 
-	public_tweets = api.search('Dogs', rpp = 20, show_user = True)
-
-	for e in public_tweets :
-		print(e)
+	for status in tweepy.Cursor(api.home_timeline).items(10) :
+            print(status.text)
